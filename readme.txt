cf53f23ea0082a23a686e6971bca46c4c2ce0379
priyanshu.mittal <priyanshu.mittal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-06 12:12:18 +0000

    Added 1.5.4 files and added readme tag
    git-svn-id: https://plugins.svn.wordpress.org/webriti-smtp-mail/tags/1.5.4@1193321 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,127 @@
+=== Easy SMTP Mail ===
+Contributors:priyanshu.mittal
+Tags:email, gmail, mail, mail smtp, outgoing mail, phpmailer, privacy, security, sendmail, smtp, ssl, tls, wordpress smtp, wp smtp, wp-phpmailer, wp_mail
+Requires at least: 3.0
+Tested up to: 4.2.2
+Stable tag: 1.5.4
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Easy SMTP Plugin help you to send Emails from your WordPress Blog using your preferred SMTP Server. 
+
+== Description ==
+
+Webriti SMTP Mail can help us to send emails via SMTP instead of the PHP mail() function. It adds a settings page to "Dashboard"->"Settings"->"Webriti SMTP Email" where you can configure the email settings.
+
+This plugin reconfigure's the wp_mail() function to send SMTP Mails. But you have to make sure that leaving Smtp Host name blank will not reconfigure the WordPress wp_mail() function.
+
+The Practical use of this plugins comes when theme have a contact page, which on submit, shows you the successful message, but, still admin did not receive any email, here comes this smtp plugin which will makes your contact form workable. = Make sure that you are using the php version 5.6.0 plus. =
+With the help of this you can make themes contact form or widget workable.
+
+**Webriti Smtp Mail Plugin has following settings.**
+
+###General Settings
+
+The general settings section consists of the following options
+
+1. From Email Address: The email address that was sending emails to users.
+2. From Name: Specify your name which you want to show in the Email Headers.
+3. SMTP Host: Your outgoing mail server (example: smtp.gmail.com)
+4. SMTP Port: The port that will be used to relay outbound mail to your mail server (example: 465).In case of TLS encryption use port 587 for gmail server.
+5. SMTP Authentication: Yes
+6. Encryption : SSL / TLS. 
+6. Username: Enter your mail server username.
+7. Password: Enter password you use to login to the mail server.
+
+###Test Mail Utility
+With this utility you can always check weither you have configured your SMTP Mail Server details correctly or not. 
+
+
+
+== Installation ==
+1. Download
+2. Upload to your `/wp-contents/plugins/` directory.
+3. Activate the plugin through the 'Plugins' menu in WordPress and Enjoy.
+
+== Screenshots ==
+
+1. Configure SMTP Settings.
+2. Help and support.
+
+
+== Frequently Asked Questions ==
+
+= How to use gmail  as your smtp server in SSL encryption mode? = 
+
+For  using Gmail Smtp Server with this plugin then set the following details
+
+* Smtp Host smtp.gmail.com
+* Smtp Port usually 465.
+* Username: Your gmail username.
+* Password: Your gmail password.
+* Encryption : SSL
+
+= How to use gmail in TLS mode? = 
+
+For  using Gmail Smtp Server with this plugin then set the following details
+
+* Smtp Host smtp.gmail.com
+* Smtp Port usually 587.
+* Username: Your gmail username.
+* Password: Your gmail password.
+* Encryption : TLS
+
+= What is the difference between SSL and TLS types of encryption's? = 
+
+TLS (Transport Layer Security) and SSL (Secure Sockets Layer) are protocols that provide data encryption and authentication between applications and servers in scenarios where that data is being sent across an insecure network, such as checking your email. 
+The terms SSL and TLS are often used interchangeably or in conjunction with each other (TLS/SSL), but one is in fact the predecessor of the other — SSL 3.0 served as the basis for TLS 1.0 which, as a result, is sometimes referred to as SSL 3.1.
+
+= Which is more secure – SSL or TLS? =
+
+It used to be believed that TLS v1.0 was marginally more secure than SSL v3.0, its predecessor.  
+However, SSL v3.0 is getting very old and recent developments, such as the POODLE vulnerability have shown that SSL v3.0 is now completely insecure (especially for web sites using it).
+[Read More](https://luxsci.com/blog/ssl-versus-tls-whats-the-difference.html "ssl vs tls").
+
+
+== Changelog ==
+
+@Version 1.5.4
+1. Recommended soem more plugins.
+
+@Version 1.5.3
+remove debug mode errros.
+= 1.5.1 =
+Typo fixed.
+= 1.5 = 
+1. Option Panel revamped.
+2. Added SMTP common error codes and suggestions.
+
+= 1.4 =
+1. Added TLS encryption mode.
+
+= 1.3 =
+1. Reply to field is now corrected
+2. Removed Filters
+= 1.2 =
+* SSL Turn off/on option added in option settings
+
+= 1.1 =
+* Small bug fixed.
+
+= 1.0.4 =
+* New Tab adding for Free wordPress Themes.
+
+= 1.0.3 =
+* Formatting not fixed in 1.0.2 is now set.
+
+= 1.0.2 =
+* Proper formatting of readme.txt file is done.
+
+= 1.0.1 =
+* Added Desctipin Text.
+
+= 1.0 =
+* Easy Smtp Mail Plugin Released
+
+== Upgrade Notice ==
+Always try to use latest version on repository.
---
+++ webriti-smtp-mail.php
@@ -0,0 +1,273 @@
+<?php
+/*
+Plugin Name: Easy SMTP Mail
+Version: 1.5.4
+Plugin URI: http://webriti.com/
+Description: the wp_mail() function to use SMTP and set your SMTP settings or your wp_mail() function no need any configuration.
+Author: priyanshu.mittal
+Author URI: http://webriti.com/
+License: GPL3
+License URI: http://www.gnu.org/licenses/gpl.html
+
+/*** The instantiated version of this plugin's class ***/
+	
+if (!function_exists('WebritiSmtpMail')) 
+{	class WebritiSmtpMail
+	{		
+		/*** This plugin's identifier ***/	 
+		const ID = 'webriti-smtp-mail';
+		
+		/*** This plugin's name ***/
+		const NAME = 'Webriti SMTP Mail';
+		
+		/*** This plugin's version ***/
+		const VERSION = '1.2';
+		// Array of options and their default values
+		
+		/*** Has the internationalization text domain been loaded?  @var bool ***/
+		public $loaded_textdomain = false;
+
+		
+		/*** Declares the WordPress action and filter callbacks ***/
+		public function __construct() 
+		{						
+				/** Define Directory Location Constants */				
+				define('WEBRITI_PLUGIN_DIR_PATH_INC', plugin_dir_path(__FILE__).'inc');
+				
+				/** Define https file Location Constants */
+				define('WEBRITI_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ));				
+				
+				// webriti smtp ui tab script 
+				$this->load_ui_tab_script();
+				
+				// Webriti smtp plugin hook file
+				$this->load_plugin_hooks_file();
+				
+				// Webriti smtp form
+				$this->webriti_smtp_form();
+				
+				// Webriti smtp textdomain
+				$this->load_plugin_textdomain();
+				
+		}		
+		/*** plugin text domain
+		 */
+		public function load_plugin_textdomain() {
+			if (!$this->loaded_textdomain) {
+				load_plugin_textdomain('webritismtp', false, self::ID . '/lang');
+			}
+		}	
+		
+		public function load_ui_tab_script() {
+	
+			//register style for ui tab 
+			function load_tab_scripts($hook) {
+			
+			if ($hook != 'settings_page_webriti_smtpmail_panels'){return;} // we dont want to load our js on other pages
+  
+			wp_register_script( 'custom_tab_js', WEBRITI_PLUGIN_DIR_URL . 'js/option-tab.js',array('jquery','jquery-ui-core','jquery-ui-tabs'), false, '1.0.0' );
+			wp_enqueue_script ('custom_tab_js');
+			
+			}
+			add_action( 'admin_enqueue_scripts', 'load_tab_scripts' );
+
+		}
+		
+		public function load_plugin_hooks_file()
+		{
+			// Webriti plugin option css and jss
+			function load_webriti_smtpmail_css_js()
+			{			
+				wp_enqueue_style('option', WEBRITI_PLUGIN_DIR_URL .'css/style-option.css');
+				wp_enqueue_style('tab_option', WEBRITI_PLUGIN_DIR_URL .'css/wff-admin.css');
+				wp_enqueue_style('bootstrap', WEBRITI_PLUGIN_DIR_URL .'css/bootstrap.css');
+			}			
+			
+			// default data set plug-in activation 
+			function webriti_smtp_activate()
+			{	$wpms_options = array (
+				'mail_from' => '', 
+				'mail_from_name' => '',
+				'mailer' => 'smtp',
+				'mail_set_return_path' => 'false',
+				'smtp_host' => 'localhost',
+				'smtp_port' => 'port',
+				'smtp_ssl' => 'ssl',
+				'smtp_auth' => 'true',
+				'smtp_user' => '',
+				'smtp_pass' => ''
+				);				
+				foreach ($wpms_options as $name => $val)
+				{	
+					add_option($name, $val);
+				}				
+			}
+			
+			function webriti_smtp_mail_from_email () 
+			{	
+				if(is_email(get_option('mail_from')))
+				{	return get_option('mail_from'); } 
+				else 
+				{
+					return get_bloginfo('admin_email');
+				}				
+				
+			} // End of webriti_wp_mail_smtp_mail_from() function definition
+			
+			function webriti_smtp_mail_from_name()
+			{
+				if(get_option('mail_from_name'))
+				{ return get_option('mail_from_name'); }
+				else
+				{ return get_bloginfo('name'); }
+			}			
+			
+			register_activation_hook(__FILE__, 'webriti_smtp_activate');				
+			
+			// Add filters to replace the mail from name and emaila ddress
+			//add_filter('wp_mail_from','webriti_smtp_mail_from');
+			
+			//add_filter('wp_mail_from_name','webriti_smtp_mail_from_name');
+			// Add an action on phpmailer_init
+			add_action('phpmailer_init','webriti_phpmailer_init_smtp');	
+			
+			// This code is copied, from wp-includes/pluggable.php as at version 2.2.2
+			function webriti_phpmailer_init_smtp($phpmailer) 
+			{	
+				// Set the mailer type as per config above, this overrides the already called isMail method
+				// Set Mailer value
+				$phpmailer->Mailer = get_option('mailer');
+				// Set From value
+				$phpmailer->From = webriti_smtp_mail_from_email();				
+				// Set FromName value
+				$phpmailer->FromName = webriti_smtp_mail_from_name();
+				// Set Reply To Field
+				$phpmailer->AddReplyTo(webriti_smtp_mail_from_email(), webriti_smtp_mail_from_name());				
+				// Set SMTPSecure value
+				$phpmailer->SMTPSecure = get_option('smtp_ssl');					
+				// Set Host value
+				$phpmailer->Host = get_option('smtp_host');				
+				// Set Port value
+				$phpmailer->Port = get_option('smtp_port');   
+				// If usrname option is not blank we have to use authentication
+				if (get_option('smtp_user') != '') {
+					$phpmailer->SMTPAuth = get_option('smtp_auth');
+					$phpmailer->Username = get_option('smtp_user');
+					$phpmailer->Password = get_option('smtp_pass');
+				}					
+				$phpmailer = apply_filters('wp_mail_smtp_custom_options', $phpmailer);
+			} // End of phpmailer_init_smtp() function definition
+			
+		}
+		
+		function webriti_smtp_form()
+		{
+			function webriti_smtp_mail_admin_menu()
+			{	
+				$menu = add_options_page( __('Webriti SMTP Mail', 'webritismtp'), __('Webriti SMTP Mail', 'webritismtp'), 'manage_options', 'webriti_smtpmail_panels', 'webriti_smtpmail_options_panels_page');				
+				add_action( 'admin_print_styles-' . $menu, 'load_webriti_smtpmail_css_js' );
+			}
+
+			// Wwbriti option page
+			function webriti_smtpmail_options_panels_page()
+			{	
+				require_once(WEBRITI_PLUGIN_DIR_PATH_INC .'/smtp-mail-option-settings.php');
+			}			
+			// admin menu hook and function
+			add_action( 'admin_menu', 'webriti_smtp_mail_admin_menu');
+		}
+	} 
+}/// class end 	
+$webritiSmtpMail = new WebritiSmtpMail; 
+
+
+/**
+ * Include the TGM_Plugin_Activation class.
+ */
+require_once dirname( __FILE__ ) . '/class-tgm-plugin-activation.php';
+ 
+add_action( 'tgmpa_register', 'my_plugin_register_required_plugins' );
+/**
+ * Register the required plugins for this theme.
+ *
+ * In this example, we register two plugins - one included with the TGMPA library
+ * and one from the .org repo.
+ *
+ * The variable passed to tgmpa_register_plugins() should be an array of plugin
+ * arrays.
+ *
+ * This function is hooked into tgmpa_init, which is fired within the
+ * TGM_Plugin_Activation class constructor.
+ */
+function my_plugin_register_required_plugins() {
+ 
+    /**
+     * Array of plugin arrays. Required keys are name and slug.
+     * If the source is NOT from the .org repo, then source is also required.
+     */
+    $plugins = array(
+ 
+       
+ 
+       
+ 
+        // This is an example of how to include a plugin from the WordPress Plugin Repository.
+        array(
+            'name'      => 'Custom Instagram Feed',
+            'slug'      => 'easy-instagram-feed',
+            'required'  => false,
+        ),
+        array(
+            'name'      => 'FaceBook Feed',
+            'slug'      => 'facebook-feed',
+            'required'  => false,
+        ),
+        array(
+            'name'      => 'Easy Coming Soon Page',
+            'slug'      => 'easy-coming-soon',
+            'required'  => false,
+        ),
+ 
+    );
+ 
+    /**
+     * Array of configuration settings. Amend each line as needed.
+     * If you want the default strings to be available under your own theme domain,
+     * leave the strings uncommented.
+     * Some of the strings are added into a sprintf, so see the comments at the
+     * end of each line for what each argument will be.
+     */
+    $config = array(
+        'default_path' => '',                      // Default absolute path to pre-packaged plugins.
+        'menu'         => 'tgmpa-install-plugins', // Menu slug.
+        'has_notices'  => true,                    // Show admin notices or not.
+        'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+        'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+        'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+        'message'      => '',                      // Message to output right before the plugins table.
+        'strings'      => array(
+            'page_title'                      => __( 'Install Recommended Plugins', 'tgmpa' ),
+            'menu_title'                      => __( 'Install Plugins', 'tgmpa' ),
+            'installing'                      => __( 'Installing Plugin: %s', 'tgmpa' ), // %s = plugin name.
+            'oops'                            => __( 'Something went wrong with the plugin API.', 'tgmpa' ),
+            'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.' ), // %1$s = plugin name(s).
+            'notice_can_install_recommended'  => _n_noop( 'This plugin recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.' ), // %1$s = plugin name(s).
+            'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ), // %1$s = plugin name(s).
+            'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
+            'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
+            'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ), // %1$s = plugin name(s).
+            'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ), // %1$s = plugin name(s).
+            'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ), // %1$s = plugin name(s).
+            'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
+            'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins' ),
+            'return'                          => __( 'Return to Required Plugins Installer', 'tgmpa' ),
+            'plugin_activated'                => __( 'Plugin activated successfully.', 'tgmpa' ),
+            'complete'                        => __( 'All plugins installed and activated successfully. %s', 'tgmpa' ), // %s = dashboard link.
+            'nag_type'                        => 'updated' // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
+        )
+    );
+ 
+    tgmpa( $plugins, $config );
+ 
+}
+?>
